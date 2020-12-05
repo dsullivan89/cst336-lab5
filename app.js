d7fb229a29205b861725773e085fcddd97c64c34
David <ds90289@yahoo.com>
2020-12-05 23:58:48 +0800

    fixed server startup crash

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.listen(PORT, IP, function() {
-    console.log(`Server is running. http://${IP}:${PORT}`);
-});
+if(port == 3000)
+{
+    app.listen(PORT, IP, function() {
+        console.log(`Server is running. http://${IP}:${PORT}`);
+    });
+}
+else
+{
+    app.listen(PORT, function() {
+        console.log(`Server is running on port ${PORT}.`);
+    });
+}
