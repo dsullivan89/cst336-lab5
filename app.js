652c3476696352ee2e3833f24432fa0e57568480
David <ds90289@yahoo.com>
2020-12-05 23:47:24 +0800

    Just finished page 6 of the lab

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 var IP = process.env.IP || "localhost";
 var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.send("it works");
+    res.render("index");
 });
 
 app.listen(PORT, IP, function() {
